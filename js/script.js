121cb1002b07252fe576f38bf9a2f2f585d16e90
Jessica Paul <paul.jessica95@gmail.com>
2021-03-17 17:25:03 -0400

    add modal content for each project

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
+console.log('JS is loaded');
 // cached elements
 const $main = $('main');
 
 
 // event handlers
-$main.on('click', '.project', modalClick);
+
 
 
 // functions
-function modalClick(evt) {
-    console.log('clicked on a project')
-}
